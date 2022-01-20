10115fc7893c9d6fd497f144e5409d9c22195d0a
Dqu1J <mcpeytwot5@gmail.com>
2022-01-20 21:02:22 +0200

    stuff

--- src/main/java/dqu/additionaladditions/AdditionalAdditionsClient.java
+++ src/main/java/dqu/additionaladditions/AdditionalAdditionsClient.java
@@ -1,8 +1,13 @@
 package dqu.additionaladditions;
 
+import dqu.additionaladditions.item.PocketJukeboxItem;
 import net.minecraft.client.renderer.ItemBlockRenderTypes;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.ThrownItemRenderer;
+import net.minecraft.client.renderer.item.ItemProperties;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CrossbowItem;
+import net.minecraft.world.item.Items;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.client.gui.IIngameOverlay;
@@ -22,6 +27,39 @@ public class AdditionalAdditionsClient {
         ItemBlockRenderTypes.setRenderLayer(AdditionalRegistry.COPPER_PATINA.get(), RenderType.cutout());
         ItemBlockRenderTypes.setRenderLayer(AdditionalRegistry.ROPE_BLOCK.get(), RenderType.cutout());
         ItemBlockRenderTypes.setRenderLayer(AdditionalRegistry.GLOW_STICK_BLOCK.get(), RenderType.cutout());
+
+        event.enqueueWork(() -> {
+            ItemProperties.register(AdditionalRegistry.CROSSBOW_WITH_SPYGLASS.get(), new ResourceLocation(AdditionalAdditions.namespace, "pull"), (stack, level, living, id) -> {
+                if (living == null) return 0.0F;
+                return living.getUseItem() != stack ? 0.0F : (stack.getUseDuration() - living.getUseItemRemainingTicks()) / 20.0F;
+            });
+
+            ItemProperties.register(AdditionalRegistry.CROSSBOW_WITH_SPYGLASS.get(), new ResourceLocation(AdditionalAdditions.namespace, "pulling"), (stack, level, living, id) -> {
+                if (living == null) return 0.0F;
+                return living.isUsingItem() && living.getUseItem() == stack ? 1.0F : 0.0F;
+            });
+
+            ItemProperties.register(AdditionalRegistry.CROSSBOW_WITH_SPYGLASS.get(), new ResourceLocation(AdditionalAdditions.namespace, "charged"), (stack, level, living, id) -> {
+                return CrossbowItem.isCharged(stack) ? 1.0F : 0.0F;
+            });
+
+            ItemProperties.register(AdditionalRegistry.CROSSBOW_WITH_SPYGLASS.get(), new ResourceLocation(AdditionalAdditions.namespace, "firework"), (stack, level, living, id) -> {
+                return CrossbowItem.containsChargedProjectile(stack, Items.FIREWORK_ROCKET) ? 1.0F : 0.0F;
+            });
+
+            ItemProperties.register(AdditionalRegistry.POCKET_JUKEBOX_ITEM.get(), new ResourceLocation(AdditionalAdditions.namespace, "disc"), (stack, level, living, id) -> {
+                return PocketJukeboxItem.hasDisc(stack) ? 1.0F : 0.0F;
+            });
+
+            ItemProperties.register(AdditionalRegistry.DEPTH_METER_ITEM.get(), new ResourceLocation(AdditionalAdditions.namespace, "angle"), (stack, level, living, id) -> {
+                if (living == null) return 0.2f;
+                int height = living.level.getMaxBuildHeight() - living.level.getMinBuildHeight();
+                double playerY = living.getY() - living.level.getMinBuildHeight();
+                double quarter = height / 4d;
+                int value = (int) (playerY / quarter);
+                return (value < 0) ? 0 : (Math.min(value, 3) / 10f);
+            });
+        });
     }
 
     @SubscribeEvent
