49e53692209a06ac8d1867fa8f58c635220ad3b1
Dqu1J <mcpeytwot5@gmail.com>
2022-05-24 17:02:52 +0300

    4.0.1-4.0.3

--- src/main/java/dqu/additionaladditions/enchantment/SpeedEnchantment.java
+++ src/main/java/dqu/additionaladditions/enchantment/SpeedEnchantment.java
@@ -19,9 +19,9 @@ public class SpeedEnchantment extends Enchantment {
 
     @Override
     protected boolean checkCompatibility(Enchantment other) {
-        if (Config.getBool(ConfigValues.ENCHANTMENT_SPEED)) return false;
-        if (other == Enchantments.SOUL_SPEED || other == Enchantments.FROST_WALKER || other == Enchantments.DEPTH_STRIDER)
+        if (other == Enchantments.SOUL_SPEED || other == Enchantments.FROST_WALKER || other == Enchantments.DEPTH_STRIDER) {
             return false;
+        }
         return super.checkCompatibility(other);
     }
 
